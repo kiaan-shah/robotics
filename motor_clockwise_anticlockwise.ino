73a6f36cbc52fefec31883257b72f6872b0a9062
kiaan-shah <kiaanshah03@gmail.com>
2024-06-05 19:59:43 -0700

    Create motor_clockwise_anticlockwise.ino

---
+++ motor_clockwise_anticlockwise.ino
@@ -0,0 +1,18 @@
+void setup()
+{
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+  delay(3000);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, LOW);
+  delay(3000);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+  delay(3000);
+}
